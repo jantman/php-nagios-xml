fdf0aea23829413b3873402532c15608f5ed7a0f
Jason Antman <jason@jasonantman.com>
2009-07-30 14:15:16 +0000

    import from MON1.jasonantman.com dated 2009-03-02

--- parseNagiosXML.php
+++ parseNagiosXML.php
@@ -1,12 +1,11 @@
 <?php
 
-// Time-stamp: "2009-03-27 01:29:50 jantman"
-// $Id: parseNagiosXML.php,v 1.1 2009/03/29 05:30:51 jantman Exp $
+// Time-stamp: "2009-01-31 22:24:57 jantman"
+// $Id: parseXML.php,v 1.2 2008/01/27 02:38:48 jantman Exp $
 
 // parses Nagios output from XML generator on MON1 into HTML
 
-#$file = "http://voip1/statusXML.php";
-$file = "statusXML.xml";
+$file = "http://voip1/statusXML.php";
 $nagios_host = "VoIP1";
 
 
@@ -17,102 +16,188 @@ $doc->load($file);
 
 $top = $doc->getElementsByTagName("nagios_status"); // returns DOMNodeList
 $hosts = $top->item(0)->getElementsByTagName("host");
+
+// program stuff
+$program = $top->item(0)->getElementsByTagName("programStatus");
+foreach($program as $elem)
+{
+    $nagios_pid = $elem->getElementsByTagName("nagios_pid")->item(0)->nodeValue;
+    $program_start = $elem->getElementsByTagName("program_start")->item(0)->nodeValue;
+    $last_command_check = $elem->getElementsByTagName("last_command_check")->item(0)->nodeValue;
+}
+
+echo '<p class="description">';
+echo '<strong>Field Descriptions:</strong><br />';
+echo '<strong>Host:</strong> Which computer (or logical grouping if not a physical computer).<br />';
+echo '<strong>Service:</strong> The specific service (i.e. web server, database server, etc.).<br />';
+echo '<strong>Status:</strong> Current status (i.e. OK, WARNing, CRITical).<br />';
+echo '<strong>Output:</strong> Output from the check plugin (only of consequence to Jason, unless it says somethng that sounds bad).<br />';
+echo '<strong>Last Check:</strong> How long ago it was last checked (most services should be 5 minutes or less).<br />';
+echo '<strong>Last Change:</strong> How long ago since Status last changed (small amounts of time could be a problem if it\'s currently OK).<br />';
+echo '<strong>Age:</strong> Age of current information (how long ago this information was grabbed from the server).<br />';
+echo '<strong>Exec Time:</strong> How long it took to check (numbers over 4-5s are a problem).<br />';
+echo '<strong>Ack:</strong> Whether a problem has been acknowledged (by Jason).<br />';
+echo '<br /><br />';
+echo '<strong>What Really Matters to You:</strong> If the Status is not "OK", that\'s probably bad. If the last column ("Ack") is not "Y" and the Status is not "OK", it means there\'s a problem that Jason doesn\'t know about. Call him. (201-906-7347). If you think there have been problems lately, but everything looks OK (or is ACKnowledged) check through the "Last Change" column - very recent times mean that there was a problem but it\'s now ok.<br />';
+echo '<p>'."\n";
+
+echo '<div class="nagiosInfo"><strong>Program Info:</strong> PID: '.$nagios_pid.' Start: '.format_ts($program_start).' ago Last Check: '.format_ts($last_command_check).' ago</div>';
+
+// TABLE SETUP
+echo '<table class="nagiosStatus">'."\n";
+echo '<tr>'."\n";
+echo '<th>Host</th><th>Service</th><th>Status</th><th>Output</th><th>Last Check</th><th>Last Change</th><th>Age</th><th>Exec Time</th><th>Ack</th>'."\n";
+echo '</tr>'."\n";
+
+$host_count = 0;
+$total_hosts = 0;
 $services = $doc->getElementsByTagName("services");
 $services = $services->item(0)->getElementsByTagName("service");
-
-$hostAry = array();
+$service_count = 0;
+$total_services = 0;
 // HOSTS
 foreach($hosts as $value)
 {
     $name = $value->getElementsByTagName("host_name")->item(0)->nodeValue;
-    $hostAry[$name] = array();
-}
+    $status = $value->getElementsByTagName("current_state")->item(0)->nodeValue;
+    $plugin_output = $value->getElementsByTagName("plugin_output")->item(0)->nodeValue;
+    $last_check = $value->getElementsByTagName("last_check")->item(0)->nodeValue;
+    $last_state_change = $value->getElementsByTagName("last_state_change")->item(0)->nodeValue;
+    $ack = $value->getElementsByTagName("problem_has_been_acknowledged")->item(0)->nodeValue;
+    $last_update = $value->getElementsByTagName("last_update")->item(0)->nodeValue;
+    $exec_time = $value->getElementsByTagName("check_execution_time")->item(0)->nodeValue;
+    if($status != 0) { $host_count++;}
 
-// SERVICES
-// parse the services
-foreach($services as $serValue)
+    host_tr($name, $status, $plugin_output, $last_check, $last_state_change, $ack, $last_update, $exec_time);
+
+    // SERVICES
+    // parse the services
+    foreach($services as $serValue)
+    {
+	$service_host = $serValue->getElementsByTagName("host_name")->item(0)->nodeValue;
+	if($service_host == $name)
+	{
+	    if($status != 0) {$service_count++;}
+	    $s_status = $serValue->getElementsByTagName("current_state")->item(0)->nodeValue;
+	    $s_plugin_output = $serValue->getElementsByTagName("plugin_output")->item(0)->nodeValue;
+	    $s_last_check = $serValue->getElementsByTagName("last_check")->item(0)->nodeValue;
+	    $s_last_state_change = $serValue->getElementsByTagName("last_state_change")->item(0)->nodeValue;
+	    $s_ack = $serValue->getElementsByTagName("problem_has_been_acknowledged")->item(0)->nodeValue;
+	    $s_last_update = $serValue->getElementsByTagName("last_update")->item(0)->nodeValue;
+	    $s_service_description = $serValue->getElementsByTagName("service_description")->item(0)->nodeValue;
+	    $s_exec_time = $value->getElementsByTagName("check_execution_time")->item(0)->nodeValue;
+	    serv_tr($s_service_description, $s_status, $s_plugin_output, $s_last_check, $s_last_state_change, $s_ack, $s_last_update, $s_exec_time);
+	    $total_services++;
+	}
+    }
+    // END SERVICES
+    $total_hosts++;
+}
+echo '</table>'."\n";
+
+
+// FUNCTIONS
+function statusColor($status_value)
 {
-    $service_host = $serValue->getElementsByTagName("host_name")->item(0)->nodeValue;
-    $s_status = $serValue->getElementsByTagName("current_state")->item(0)->nodeValue;
-    $s_plugin_output = $serValue->getElementsByTagName("plugin_output")->item(0)->nodeValue;
-    $s_last_check = $serValue->getElementsByTagName("last_check")->item(0)->nodeValue;
-    $s_last_state_change = $serValue->getElementsByTagName("last_state_change")->item(0)->nodeValue;
-    $s_ack = $serValue->getElementsByTagName("problem_has_been_acknowledged")->item(0)->nodeValue;
-    $s_last_update = $serValue->getElementsByTagName("last_update")->item(0)->nodeValue;
-    $s_service_description = $serValue->getElementsByTagName("service_description")->item(0)->nodeValue;
-    $s_exec_time = $value->getElementsByTagName("check_execution_time")->item(0)->nodeValue;
-    $temp = array();
-    $temp['status'] = $s_status;
-    $temp['last_check'] = $s_last_check;
-    $temp['ack'] = $s_ack;
-    $hostAry[$service_host][$s_service_description] = $temp;
+    switch($status_value)
+    {
+	case 0:
+	    return "#00FF00";
+	case 1:
+	    return "#FFFF00";
+	case 2:
+	    return "#FE2E2E";
+    }
 }
 
-// start making the strings
-$line1 = "PCRserv, MPAC-SW1 OK";
-$line2 = "VoIP1 OK";
-$line3 = "WAN, Asterisk OK";
-$line4 = "";
-$led1 = 0; // 0==green, 1==orange, 2==red
-$led2 = 0;
-$led3 = 0;
-$led4 = 0;
-
-// higher priorities overwrite lower priorities
-
-// SW1 - L1 Pri 1
-if(($hostAry['MPAC-SW1']['Uptime']['status'] != 0 && $hostAry['MPAC-SW1']['Uptime']['ack'] == 0) || ($hostAry['MPAC-SW1']['PING']['status'] != 0 && $hostAry['MPAC-SW1']['PING']['ack'] == 0))
+function ackTD($ack, $is_a_host)
 {
-    $led1 = 1;
-    $line1 = str_pad("MPAC-SW1 FAIL", 15).chr(160).date("Hi", $hostAry['MPAC-SW1']['PING']['last_check']);
+    if($is_a_host){ $host = 'class="hostRow"';}else{$host = "";}
+    switch($ack)
+    {
+	case 0:
+	    return '<td class="statusCRIT" '.$host.'>N</td>'."\n";
+	case 1:
+	    return '<td class="statusOK" '.$host.'>Y</td>'."\n";
+    }
 }
 
-// PCRserv SW - L1 Pri 2
-if(($hostAry['PCRserv']['Check HTTP External If']['status'] != 0 && $hostAry['PCRserv']['Check HTTP External If']['ack'] == 0) || ($hostAry['PCRserv']['Check HTTP Internal If']['status'] != 0 && $hostAry['PCRserv']['Check HTTP Internal If']['ack'] == 0) || ($hostAry['PCRserv']['Load Average']['status'] != 0 && $hostAry['PCRserv']['Load Average']['ack'] == 0) || ($hostAry['PCRserv']['MySQLd']['status'] != 0 && $hostAry['PCRserv']['MySQLd']['ack'] == 0) || ($hostAry['PCRserv']['Total Processes']['status'] != 0 && $hostAry['PCRserv']['Total Processes']['ack'] == 0))
+function format_ts($ts)
 {
-    $led1 = 2;
-    $line1 = str_pad("PCRserv SW FAIL", 15).chr(160).date("Hi", $hostAry['PCRserv']['MySQLd']['last_check']);
+    // formats timeticks into a textual representation
+    $seconds = time() - $ts;
+    $final = "";
+    if($seconds > 86400) // > 1 day
+    {
+	$days = (int)($seconds / 86400);
+	$final .= $days."d ";
+	$seconds = ((int)$seconds % 86400);
+    }
+    if($seconds > 3600) // 1 hour
+    {
+	$hours = (int)($seconds / 3600);
+	$final .= $hours."h ";
+	$seconds = (int)($seconds % 3600);
+    }
+    if($seconds > 60) // 1 minute
+    {
+	$min = (int)($seconds / 60);
+	$final .= $min."m ";
+	$seconds = (int)($seconds % 60);
+    }
+    $final .= $seconds."s";
+    return $final;
 }
 
-// PCRserv HW - L1 Pri 3
-if(($hostAry['PCRserv']['Fans']['status'] != 0 && $hostAry['PCRserv']['Fans']['ack'] == 0) || ($hostAry['PCRserv']['Power Supplies']['status'] != 0 && $hostAry['PCRserv']['Power Supplies']['ack'] == 0) || ($hostAry['PCRserv']['Processors']['status'] != 0 && $hostAry['PCRserv']['Processors']['ack'] == 0) || ($hostAry['PCRserv']['Temperatures']['status'] != 0 && $hostAry['PCRserv']['Temperatures']['ack'] == 0))
+function host_tr($name, $status, $plugin_output, $last_check, $last_state_change, $ack, $last_update, $exec_time)
 {
-    $led1 = 2;
-    $line1 = str_pad("PCRserv HW FAIL", 15).chr(160).date("Hi", $hostAry['PCRserv']['MySQLd']['last_check']);
+    echo '<tr class="hostRow">';
+    echo '<td class="hostRow"><strong>'.$name.'</strong></td>';
+    echo '<td class="hostRow">(host)</td>';
+    echo statusTD($status, true);
+    echo '<td class="hostRow">'.$plugin_output.'</td>';
+    echo '<td class="hostRow">'.format_ts($last_check).'</td>';
+    echo '<td class="hostRow">'.format_ts($last_state_change).'</td>';
+    echo '<td class="hostRow">'.format_ts($last_update).'</td>';
+    echo '<td class="hostRow">'.$exec_time.'s</td>';
+    if($status != 0){ echo ackTD($ack, true);} else { echo '<td class="hostRow">&nbsp;</td>';}
+    echo '</tr>';
 }
 
-// VoIP1 - Asterisk - L3 Pri 3
-if(($hostAry['VoIP1']['Asterisk - LesNet Peer']['status'] != 0 && $hostAry['VoIP1']['Asterisk - LesNet Peer']['ack'] == 0) || ($hostAry['VoIP1']['Asterisk - Mgr']['status'] != 0 && $hostAry['VoIP1']['Asterisk - Mgr']['ack'] == 0) || ($hostAry['VoIP1']['Asterisk Proc Running']['status'] != 0 && $hostAry['VoIP1']['Asterisk Proc Running']['ack'] == 0))
+function serv_tr($description, $status, $plugin_output, $last_check, $last_state_change, $ack, $last_update, $exec_time)
 {
-    $led3 = 1;
-    $line3 = str_pad("VoIP1 AST FAIL", 15).chr(160).date("Hi", $hostAry['VoIP1']['Asterisk - LesNet Peer']['last_check']);
+    echo '<tr>';
+    echo '<td>&nbsp;</td>';
+    echo '<td>'.$description.'</td>';
+    echo statusTD($status, false);
+    echo '<td>'.$plugin_output.'</td>';
+    echo '<td>'.format_ts($last_check).'</td>';
+    echo '<td>'.format_ts($last_state_change).'</td>';
+    echo '<td>'.format_ts($last_update).'</td>';
+    echo '<td>'.$exec_time.'s</td>';
+    if($status != 0){ echo ackTD($ack, false);} else { echo "<td>&nbsp;</td>";}
+    echo '</tr>';
 }
 
-// VoIP1 SW - L2 Pri 1
-if(($hostAry['VoIP1']['Current Load']['status'] != 0 && $hostAry['VoIP1']['Current Load']['ack'] == 0) || ($hostAry['VoIP1']['PING']['status'] != 0 && $hostAry['VoIP1']['PING']['ack'] == 0) || ($hostAry['VoIP1']['Swap Usage']['status'] != 0 && $hostAry['VoIP1']['Swap Usage']['ack'] == 0))
+function statusTD($status, $is_a_host)
 {
-    $led2 = 1;
-    $line2 = str_pad("VoIP1 SW FAIL", 15).chr(160).date("Hi", $hostAry['VoIP1']['Current Load']['last_check']);
+    if($is_a_host){ $host = 'class="hostRow"';}else{$host = "";}
+    if($status == 0)
+    {
+	return '<td class="statusOK" '.$host.'>OK</td>';
+    }
+    elseif($status == 1)
+    {
+	return '<td class="statusWARN" '.$host.'>WARN</td>';
+    }
+    elseif($status == 2)
+    {
+	return '<td class="statusCRIT" '.$host.'>CRIT</td>';
+    }
+    else
+    {
+
+    }
 }
-
-// VoIP1 HW - L2 Pri 2
-if(($hostAry['VoIP1']['Power Supplies']['status'] != 0 && $hostAry['VoIP1']['Power Supplies']['ack'] == 0) || ($hostAry['VoIP1']['Processors']['status'] != 0 && $hostAry['VoIP1']['Processors']['ack'] == 0) || ($hostAry['VoIP1']['DIMMs']['status'] != 0 && $hostAry['VoIP1']['DIMMs']['ack'] == 0) || ($hostAry['VoIP1']['Fans']['status'] != 0 && $hostAry['VoIP1']['Fans']['ack'] == 0) || ($hostAry['VoIP1']['Temperatures']['status'] != 0 && $hostAry['VoIP1']['Temperatures']['ack'] == 0))
-{
-    $led2 = 2;
-    $line2 = str_pad("VoIP1 HW FAIL", 15).chr(160).date("Hi", $hostAry['VoIP1']['Current Load']['last_check']);
-}
-
-//WAN
-if(($hostAry['wantest']['PING Google']['status'] != 0 && $hostAry['wantest']['PING Google']['ack'] == 0) || ($hostAry['wantest']['PING OptOnline.net']['status'] != 0 && $hostAry['wantest']['PING OptOnline.net']['ack'] == 0) || ($hostAry['wantest']['PING Rutgers']['status'] != 0 && $hostAry['wantest']['PING Rutgers']['ack'] == 0) || ($hostAry['wantest']['PING Yahoo']['status'] != 0 && $hostAry['wantest']['PING Yahoo']['ack'] == 0))
-{
-    $led3 = 2;
-    $line3 = str_pad("WAN Conn FAIL", 15).chr(160).date("Hi", $hostAry['wantest']['PING Google']['last_check']);
-}
-
-// we now have an array of the information for the first 3 lines and LEDs
-
-echo $led1.",".$line1."\n";
-echo $led2.",".$line2."\n";
-echo $led3.",".$line3."\n";
 
 ?>
--- statusXML.php
+++ statusXML.php
@@ -1,8 +1,37 @@
 <?php
+// statusXML.php
+//
+// Script to parse Nagios status.dat and present it as XML, to allow inclusion
+//   of status info in a web page served from a separate machine.
+//
+// +----------------------------------------------------------------------+
+// | PHP EMS Tools      http://www.php-ems-tools.com                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2006, 2007 Jason Antman.                               |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 3 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// | Authors: Jason Antman <jason@jasonantman.com>                        |
+// +----------------------------------------------------------------------+
+//      $Id: statusXML.php,v 1.6 2009/02/07 03:32:14 jantman Exp $
+//      $Source: /usr/local/cvsroot/admin-portal/nagios/statusXML.php,v $
 
-// this script mines data from the status.dat of a Nagios 2.x installation
-
-$statusFile = "/var/lib/nagios/status.dat";
+$statusFile = "/usr/local/nagios/var/status.dat";
 
 $nag_version = getFileVersion($statusFile); // returns integer 2 or 3
 $created_ts = 0;
